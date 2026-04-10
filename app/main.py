4881780db50c60eb03ef3e9e8ba4376acde8b2d0
kauadp <kauadp1405@gmail.com>
2026-04-10 18:48:19 -0300

    Corrige Range

--- app/main.py
+++ app/main.py
@@ -39,7 +39,7 @@ BOOL_TRUE = {"SIM", "S", "TRUE", "1", "RECORRENTE"}
 BOOL_FALSE = {"NAO", "NÃO", "N", "FALSE", "0", "NOVO"}
 
 # Limites superiores exclusivos para uso com iloc[start:end]
-STAND_ES_END_ROW = 174
+STAND_ES_END_ROW = 175
 FOOD_ES_END_ROW = 42
 STAND_RJ_END_ROW = 102
 
